fc5eaf61515decbe4947a73aa4b54dc515963afe
arcaeon <65035087+arcaeon@users.noreply.github.com>
2020-07-01 00:42:48 -0400

    Add ObservableObject class

--- Solarus.Mvvm/ViewModelBase.cs
+++ Solarus.Mvvm/ViewModelBase.cs
@@ -1,36 +1,13 @@
-﻿using System.Collections.Generic;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
-
-namespace Solarus.Mvvm
+﻿namespace Solarus.Mvvm
 {
-    public abstract class ViewModelBase : INotifyPropertyChanged
+    public abstract class ViewModelBase : ObservableObject
     {
         private bool _isLoading;
-
-        public event PropertyChangedEventHandler PropertyChanged;
 
         public bool IsLoading
         {
             get { return _isLoading; }
             set { SetProperty(ref _isLoading, value); }
-        }
-
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
-        protected virtual bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
-        {
-            if (EqualityComparer<T>.Default.Equals(storage, value))
-            {
-                return false;
-            }
-
-            storage = value;
-            OnPropertyChanged(propertyName);
-            return true;
         }
     }
 }
