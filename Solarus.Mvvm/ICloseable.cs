28a3c2fbca1808b4a5098ef528495eb00c67a461
arcaeon <65035087+arcaeon@users.noreply.github.com>
2020-07-13 12:36:48 -0400

    Update ICloseable

--- Solarus.Mvvm/ICloseable.cs
+++ Solarus.Mvvm/ICloseable.cs
@@ -4,8 +4,6 @@ namespace Solarus.Mvvm
 {
     public interface ICloseable
     {
-        event EventHandler AcceptRequested;
-        event EventHandler CancelRequested;
         event EventHandler CloseRequested;
     }
 }
