aed80ba317d9e3e852ff99262390380786cf9aaf
arcaeon <65035087+arcaeon@users.noreply.github.com>
2020-07-13 19:19:25 -0400

    Update IDialogModel inheritance

--- Solarus.Mvvm/IDialogModel.cs
+++ Solarus.Mvvm/IDialogModel.cs
@@ -2,7 +2,7 @@
 
 namespace Solarus.Mvvm
 {
-    public interface IDialogModel : ICloseable
+    public interface IDialogModel : ICloseable, IViewModel
     {
         event EventHandler AcceptRequested;
         event EventHandler CancelRequested;
