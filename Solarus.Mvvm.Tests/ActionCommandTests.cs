43bbcaadce1c0f3f6553df549dcacad73ecb0651
arcaeon <65035087+arcaeon@users.noreply.github.com>
2021-01-03 14:58:25 -0500

    Add ActionCommandTests

---
+++ Solarus.Mvvm.Tests/ActionCommandTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Solarus.Mvvm.Tests
+{
+    [TestFixture]
+    public class ActionCommandTests
+    {
+        [Test]
+        public void ActionCommand_WhenActionIsNull_ThrowsArgumentNullException()
+        {
+            ActionCommand command;
+            Assert.Throws<ArgumentNullException>(() => command = new ActionCommand(null));
+        }
+    }
+}
